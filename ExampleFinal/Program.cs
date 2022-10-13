fe4567bffe6d020e791a302144f5257e1b10245f
Nikita Pletnev <betpezeh1@yahoo.com>
2022-10-13 13:21:57 +0500

    add new coments Programm

--- ExampleFinal/Program.cs
+++ ExampleFinal/Program.cs
@@ -8,25 +8,31 @@ string[] result = FindLessThan(array, 3);//строковый массив (со
 WriteLine($"[{string.Join(", ", array)}] -> [{string.Join(", ", result)}]");//ввыод через интерполяцию строк 
 //изначально формируем массив, заданный пользователем, затем производим выборку значений из указанного массива (не более трех символов)
 
-string[] FindLessThan(string[] input, int n) //создание метода перебора всех элементов в массиве
+string[] FindLessThan(string[] input, int n) //метод перебора всех элементов в массиве с провекрой условия (n = 3)
 {
-    string[] output = new string[CountLessThan(input, n)];
+    string[] output = new string[CountLessThan(input, n)];//вызов метода с условием (n = 3)
 
-    for(int i = 0, j = 0; i < input.Length; i++) {
-        if(input[i].Length <= n) {
+    for(int i = 0, j = 0; i < input.Length; i++) // цикл по которому проверяем все элемменты массива
+    {
+        if(input[i].Length <= n) /*Условие при котором (n = 3), а значит если элемент массива меньше либо равен 3 символам 
+        переносим этот элемент в финальный массив, и так пока не проверим все элементы исходного массива*/
+        {
             output[j] = input[i];
             j++;
         }
     }
 
-    return output;
+    return output;//возвращаем значение 
 }
 
-int CountLessThan(string[] input, int n) {
+int CountLessThan(string[] input, int n) // метод перебора всех элементов массива
+{
     int count = 0;
 
-    for(int i = 0; i < input.Length; i++) {
-        if(input[i].Length <= n) {
+    for(int i = 0; i < input.Length; i++) 
+    {
+        if(input[i].Length <= n) 
+        {
             count++;
         }
     }
@@ -34,7 +40,8 @@ int CountLessThan(string[] input, int n) {
     return count;
 }
 
-string[] AskArray() {
+string[] AskArray() //создание исходного массива, вводим с клавиатуры через пробел
+{
     Write("Введите значения через пробел: ");
     return ReadLine().Split(" ");
 }
